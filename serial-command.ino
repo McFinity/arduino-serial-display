b5783f25c640b1d9a8a2be598999f95a6b0f2ad8
shane <shane@shane.com>
2020-05-08 10:02:37 -0500

    Seems to be working sans the NeoPixel code

--- serial-command.ino
+++ serial-command.ino
@@ -1,10 +1,60 @@
-#include "command.h"
-#include <Adafruit_NeoPixel.h>
 #define LED_PIN 6
 #define HEIGHT 16
 #define WIDTH 16
 
-Adafruit_NeoPixel strip = Adafruit_NeoPixel(HEIGHT * WIDTH, LED_PIN, NEO_GRB + NEO_KHZ800);
+const int NULL_COMMAND_CODE = 0;
+
+struct Message
+{
+    uint16_t bufferSize;
+    int *buffer;
+    void trash () {
+        free(buffer);
+    }
+    Message () {
+        bufferSize = 0;
+        buffer = (int *) malloc(0);
+    }
+};
+
+struct Command
+{
+    int commandCode = NULL_COMMAND_CODE;
+    int *data;
+    uint16_t dataIndex = 0;
+    uint16_t lastIndex = 0;
+    bool initialized = false;
+    void initData (uint16_t expectedByteCount) {
+        free(data);
+        data = (int*) malloc(sizeof(int) * expectedByteCount);
+        //memset(data, 0, sizeof(int) * expectedByteCount);
+        dataIndex = 0;
+        lastIndex = expectedByteCount - 1;
+        initialized = true;
+    }
+    void addByte (int byte) {
+        if (initialized && dataIndex <= lastIndex) {
+            data[dataIndex++] = byte;
+        } 
+    }
+    void trash () {
+        if (initialized) {
+            free(data);
+        }
+    }
+    Command (Message message) {
+        data = (int*) malloc(0);
+        if (message.bufferSize > 0) {
+            commandCode = message.buffer[0];
+        }
+    }
+    Command (int code) {
+        data = (int*) malloc(0);
+        commandCode = code;
+    }
+};
+
+//Adafruit_NeoPixel strip = Adafruit_NeoPixel(HEIGHT * WIDTH, LED_PIN, NEO_GRB + NEO_KHZ800);
 
 const int SHOW_COMMAND_CODE = 100;
 const uint16_t BYTE_COUNT_FOR_BITMAP = 768;
@@ -16,10 +66,10 @@ Command activeCommand = NULL_COMMAND;
 void setup()
 {
     Serial.begin(9600);
-    strip.begin();
-    strip.setBrightness(32);
-    strip.setPixelColor(0, strip.Color(255, 255, 255));
-    strip.show();
+    // strip.begin();
+    // strip.setBrightness(32);
+    // strip.setPixelColor(0, strip.Color(255, 255, 255));
+    // strip.show();
 }
 
 void loop()
@@ -62,7 +112,7 @@ void executeCommand(Message message)
 {
     Serial.print("Command Code ");
     Serial.println(activeCommand.commandCode);
-    if (activeCommand.commandCode != NULL_COMMAND_CODE) {
+    if (activeCommand.commandCode == SHOW_COMMAND_CODE) {
         Serial.print("Command initialized ");
         Serial.println(activeCommand.initialized);
         if (!activeCommand.initialized)
@@ -90,9 +140,16 @@ void executeCommand(Message message)
             int r = activeCommand.data[i];
             int g = activeCommand.data[i + 1];
             int b = activeCommand.data[i + 2];
-            strip.setPixelColor(pixelIndex++, strip.Color(r, g, b));
+            if (i < 3) {
+            Serial.println("Color");
+            Serial.println(r);
+            Serial.println(g);
+            Serial.println(b);
+            }
+
+            // strip.setPixelColor(pixelIndex++, strip.Color(r, g, b));
         }
-        strip.show();
+        // strip.show();
 
         activeCommand.trash();
         activeCommand = NULL_COMMAND;
